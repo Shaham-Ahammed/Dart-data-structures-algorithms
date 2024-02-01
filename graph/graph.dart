239d8b7c695006f21be5812e40b5339177cf0d20
shaham ahammed p k <shahamahammed66@gmail.com>
2024-02-01 23:04:40 +0530

    added dfs and bfs and deletion

--- graph/graph.dart
+++ graph/graph.dart
@@ -19,8 +19,55 @@ class Graph {
     }
   }
 
+  delete(int vertex) {
+    if (map.containsKey(vertex)) {
+      map.remove(vertex);
+    }
+    map.forEach((key, value) {
+      if (value.contains(vertex)) {
+        value.remove(vertex);
+      }
+    });
+  }
+
   display() {
     print(map);
+  }
+
+  //breadth first search
+  bfs(int firstElement) {
+    List<int> visited = [];
+    List<int> queue = [];
+    visited.add(firstElement);
+    queue.add(firstElement);
+
+    while (queue.isNotEmpty) {
+      int currentElement = queue.removeAt(0);
+      print("${currentElement} ");
+
+      for (int element in map[currentElement]!) {
+        if (!visited.contains(element)) {
+          visited.add(element);
+          queue.add(element);
+        }
+      }
+    }
+  }
+
+  //deapth first search
+  dfs(int firstElement) {
+    List<int> visited = [];
+    _dfsHelper(firstElement, visited);
+  }
+
+  _dfsHelper(int currentVertex, List<int> visited) {
+    visited.add(currentVertex);
+    print("${currentVertex} ");
+    for (int element in map[currentVertex]!) {
+      if (!visited.contains(element)) {
+        _dfsHelper(element, visited);
+      }
+    }
   }
 }
 
@@ -28,5 +75,11 @@ void main(List<String> args) {
   Graph graph = Graph();
   graph.insert(12, 3, false);
   graph.insert(12, 5, true);
+  graph.insert(3, 7, true);
+  graph.insert(7, 9, true);
+  // graph.delete(3);
   graph.display();
+  graph.bfs(12);
+  print("-----");
+  graph.dfs(12);
 }
