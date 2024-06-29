049cff25690ce121da3c8ab2c2d4adc6c457869d
SHAHAM AHAMMED P K <shahamahammed66@gmail.com>
2024-06-29 12:10:47 +0530

    reversing a singly linked list

--- linked list/singly_linked_list.dart
+++ linked list/singly_linked_list.dart
@@ -125,7 +125,20 @@ class LinkedList {
       return;
     }
   }
-
+   
+  //reversing a singly ll
+    reversing() {
+    Node? prev = null;
+    Node? current = head;
+    Node? next = null;
+    while (current != null) {
+      next = current.next;
+      current.next = prev;
+      prev = current;
+      current = next;
+    }
+    head = prev;
+  } 
   
 }
 
